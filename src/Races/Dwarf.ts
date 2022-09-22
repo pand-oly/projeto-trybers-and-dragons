c2333d3a43018f0ec9a4867595823654d6617554
pand-oly <hoa98hoa@gmail.com>
2022-09-22 17:16:12 -0300

    feat: req 02 created classes Dwarf, Elf, Halfling, Orc

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,18 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _countInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarf._countInstances += 1;
+  }
+
+  public get maxLifePoints(): number { return this._maxLifePoints; }
+
+  static createdRacesInstances(): number {
+    return this._countInstances;
+  }
+}
